df3aa26ab0d372ca97b35ddc7d1b269764f88bc3
Samridh Srivastava <samridh@gmail.com>
2023-01-24 15:14:49 +0530

    print and insert at beginning for DLL (java)

---
+++ Java/src/linkedListCode/DoublyLinkedList.java
@@ -0,0 +1,50 @@
+package linkedListCode;
+
+public class DoublyLinkedList {
+
+    Node head;
+    static class Node{
+        int data;
+        Node prev;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+            prev = null;
+        }
+    }
+
+    static void insertAtBeginning(DoublyLinkedList list, int d){
+        Node new_node = new Node(d);
+        if(list.head==null){
+            list.head = new_node;
+            new_node.next = null;
+        }
+        else{
+            new_node.next = list.head;
+            new_node.prev = null;
+            list.head = new_node;
+        }
+
+    }
+
+    static void printDLL(DoublyLinkedList list){
+        Node curr_node = list.head;
+        while(curr_node!=null){
+            System.out.print(curr_node.data + " ");
+            curr_node = curr_node.next;
+        }
+        System.out.println(" ");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList test_list = new DoublyLinkedList();
+        insertAtBeginning(test_list, 5);
+        insertAtBeginning(test_list, 6);
+        insertAtBeginning(test_list, 7);
+        insertAtBeginning(test_list, 8);
+        printDLL(test_list);
+
+    }
+
+}
